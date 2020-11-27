ee4e3098bcf6225549a2f367c9af79825960f74f
amongash <amongash08@gmail.com>
2020-11-27 12:07:29 +0300

    programmatically add shopping data to firestore collections

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -37,6 +37,19 @@ if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
 }
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+  return await batch.commit();
+};
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
